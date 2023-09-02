909e637817a4e5cad4f6a7a50ab913d2c54e7978
Sankar சங்கர் <sankar.curiosity@gmail.com>
2023-09-02 12:09:33 +0530

    more changeS

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"embed"
+	"io/fs"
 	"log"
 	"net/http"
 )
 
+// go:embed public
+var public embed.FS
+
 func main() {
+	// cd to the public directory
+	publicFS, err := fs.Sub(public, "public")
+	if err != nil {
+		log.Fatal(err)
+	}
+	http.Handle("/embedded/", http.StripPrefix("/embedded/",
+		http.FileServer(http.FS(publicFS))))
+
 	http.Handle("/public/",
 		http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
 
